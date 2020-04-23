74d5d38ce3a5927223389fceab51153ff5578890
Michael Adkins <mikeadkins1112@gmail.com>
2020-04-23 16:59:05 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -82,8 +82,7 @@ def adding_matz
         }
      }
   programmer_hash[:yukihiro_matsumoto]={:known_for => "Ruby", :languages => ["LISP", "C"]}
-  programmer_hash[:alan_kay][:known_for]="GUI"
-  programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+
   programmer_hash
 end
 
@@ -105,8 +104,10 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
      alans_new_info = "GUI"
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
 
-
+     programmer_hash
+     
 end
 
 def adding_to_dennis
@@ -125,5 +126,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+     programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+    programmer_hash
 end
